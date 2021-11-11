02659a2668adeb6b08187d9f6719e953a798c0a8
TarikCata <83212342+TarikCata@users.noreply.github.com>
2021-11-12 00:52:50 +0100

    Commit

---
+++ Prva_Parcijala_2018/Prva_Parcijala_2018/FrmStudentNovii.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Prva_Parcijala_2018
+{
+    public partial class FrmStudentNovii : Form
+    {
+        private string domain = "@edu.fit.ba";
+
+        private static List<Predmet> predmeti;
+
+        public static double _Prosjek { get; set; }
+
+        static int _brojPolozenih { get; set; } = 0;
+
+        public FrmStudentNovii() 
+        {
+            InitializeComponent();
+        }
+
+        private void FrmStudentDetalji_Load(object sender, EventArgs e)
+        {
+            txtEmail.Text = $"ib{Student.Br}{domain}";
+            txtBrojIndeksa.Text = txtEmail.Text.Trim(domain.ToCharArray());
+        }
+        private static List<Predmet> GenerisiPredmete()
+        {
+            int brojac = 1;
+            double sum = 0;
+            predmeti = new List<Predmet>();
+            for (int i = 0; i < 5; i++)
+            {
+                Random r = new Random();
+                var p = new Predmet()
+                {
+                    Naziv = $"Predmet {brojac}",
+                    Ocjena = r.Next(6, 10),
+                    _DateTime = DateTime.Now.ToUniversalTime(),
+                };
+                predmeti.Add(p);
+                brojac++;
+                sum += p.Ocjena;
+                _brojPolozenih++;
+            }
+            _Prosjek = Math.Round((sum) / (predmeti.Count), 2);
+            return predmeti;
+        }
+        
+        private void btnSpasi_Click(object sender, EventArgs e)
+        {
+            
+            if (Validacija.Validiraj(txtIme,err) && Validacija.Validiraj(txtPrezime,err) && 
+                Validacija.Validiraj(pbStudentSlika,err))
+            {
+                var s = new Student()
+                {  
+                    Ime = txtIme.Text,
+                    Prezime = txtPrezime.Text,
+                    Email = txtEmail.Text,
+                    Predmeti = GenerisiPredmete(),
+                    image = pbStudentSlika.Image,
+                    Prosjek  = _Prosjek,
+                    BrojPolozenih  = _brojPolozenih,
+                };
+                _brojPolozenih = 0;
+                InMemoryDb.Registrovani.Add(s);
+                MessageBox.Show("Dodan");
+                Close();
+                return;
+            }
+        }
+
+        private void pbStudentSlika_Click(object sender, EventArgs e)
+        {
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string putanja = ofd.FileName;
+                Image image = Image.FromFile(putanja);
+                pbStudentSlika.Image = image;
+            }
+        }
+    }
+}
---
+++ Prva_Parcijala_2018/Prva_Parcijala_2018/InMemoryDb.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Prva_Parcijala_2018
+{
+    internal class InMemoryDb
+    {
+        public static List<Student> Registrovani { get; set; }
+        public static List<Predmet> Predmeti { get; set; } = GenerisiPredmete();
+
+        public double Prosjek { get; set; }
+        private static List<Predmet> GenerisiPredmete()
+        {
+            Predmeti = new List<Predmet>();
+            int brojac = 1;
+            for (int i = 0; i < 5; i++)
+            {
+                Random r = new Random();
+                var p = new Predmet()
+                {
+                    Naziv = $"Predmet {brojac}",
+                    Ocjena = r.Next(6, 10),
+                    _DateTime = DateTime.Now.ToUniversalTime(),
+                };
+                Predmeti.Add(p);
+                brojac++;
+            }
+            return Predmeti;
+        }
+
+        static InMemoryDb()
+        {
+            Registrovani = new List<Student>();
+            DodajDefault();
+        }
+
+        private static void DodajDefault()
+        {
+            Student s1 = new Student()
+            {
+                Ime = "Denis",
+                Prezime = "Mušić",
+                Email = "denis@edu.fit.ba",
+            };
+            Student s2 = new Student()
+            {
+                Ime = "Elmir",
+                Prezime = "Babović",
+                Email = "elmir@edu.fit.ba",
+            };
+            Student s3 = new Student()
+            {
+                Ime = "Emina",
+                Prezime = "Junuz",
+                Email = "emina@edu.fit.ba",
+            };
+
+            s1.DodajOcjenu("Matematika 1", 10);
+            s1.DodajOcjenu("Programiranje 1", 10);
+            s1.DodajOcjenu("Programiranje 2", 10);
+            s1.DodajOcjenu("Programiranje 3", 10);
+            s1.Restart();
+
+            s2.DodajOcjenu("Programiranje 1", 10);
+            s2.DodajOcjenu("Programiranje 2", 6);
+            s2.DodajOcjenu("Programiranje 3", 6);
+            s2.DodajOcjenu("Programiranje 4", 6);
+            s2.DodajOcjenu("Programiranje 5", 10);
+            s2.Restart();
+
+            s3.DodajOcjenu("Programiranje 6", 10);
+            s3.DodajOcjenu("Programiranje 7", 9);
+            s3.DodajOcjenu("Programiranje 8", 6);
+            s3.DodajOcjenu("Programiranje 9", 7);
+            s3.DodajOcjenu("Programiranje 10", 6);
+            s3.Restart();
+
+            Registrovani.Add(s1);
+            Registrovani.Add(s2);
+            Registrovani.Add(s3);
+        }
+
+        
+    }
+}
