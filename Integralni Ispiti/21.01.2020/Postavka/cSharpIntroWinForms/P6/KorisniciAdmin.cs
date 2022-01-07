9777e321c40d66117a8bea77207cc5ef56e18e95
TarikCata <83212342+TarikCata@users.noreply.github.com>
2022-01-07 10:33:46 +0100

    First commit

---
+++ Integralni Ispiti/21.01.2020/Postavka/cSharpIntroWinForms/P6/KorisniciAdmin.cs
@@ -0,0 +1,95 @@
+﻿using cSharpIntroWinForms.P10;
+using cSharpIntroWinForms.P8;
+using cSharpIntroWinForms.P9;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cSharpIntroWinForms
+{
+    public partial class KorisniciAdmin : Form
+    {
+
+        KonekcijaNaBazu konekcijaNaBazu = DLWMS.DB;
+
+        public KorisniciAdmin()
+        {
+            InitializeComponent();
+            dgvKorisnici.AutoGenerateColumns = false;
+        }
+
+        private void KorisniciAdmin_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+
+
+        private void LoadData(List<Korisnik> korisnici = null)
+        {
+            try
+            {
+                List<Korisnik> rezultati = korisnici ?? konekcijaNaBazu.Korisnici.ToList();
+
+                dgvKorisnici.DataSource = null;
+                dgvKorisnici.DataSource = rezultati;
+                if (korisnici != null)
+                    ucitajProsjek(korisnici);
+                else
+                    ucitajProsjek(konekcijaNaBazu.Korisnici.ToList());
+
+            }
+            catch (Exception ex)
+            {
+                MboxHelper.PrikaziGresku(ex);
+            }
+        }
+
+        private void ucitajProsjek(List<Korisnik> korisnici)
+        {
+            double sum = 0;
+            int brojPredmete = 0;
+            foreach (var korisnik in korisnici)
+            {
+                foreach (var predmet in korisnik.Uspjeh)
+                {
+                    sum += predmet.Ocjena;
+                    brojPredmete++;
+                }
+            }
+            if(sum > 0)
+                lblProsjek.Text = $"Prosjek ocjena: {Math.Round(sum / brojPredmete,2)}";
+            else
+                lblProsjek.Text = $"Prosjek ocjena: 0";
+        }
+
+        private void txtPretraga_TextChanged(object sender, EventArgs e)
+        {
+            List<Korisnik> korisniks = new List<Korisnik>();
+            var txt = txtPretraga.Text.ToLower();
+            foreach (var x in konekcijaNaBazu.Korisnici.ToList())
+            {
+                if(x.Ime.ToLower().Contains(txt) || x.Prezime.ToLower().Contains(txt))
+                    korisniks.Add(x);
+            }
+            LoadData(korisniks);
+        }
+
+        private void dgvKorisnici_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.ColumnIndex == 5)
+            {
+                this.Hide();
+                var obj = dgvKorisnici.CurrentRow.DataBoundItem as Korisnik;
+                new KorisniciPolozeniPredmeti(obj).ShowDialog();
+                this.Show();
+            }
+        }
+    }
+}
---
+++ Integralni Ispiti/21.01.2020/Postavka/cSharpIntroWinForms/P8/KorisniciPolozeniPredmeti.cs
@@ -0,0 +1,137 @@
+﻿using cSharpIntroWinForms.P10;
+using cSharpIntroWinForms.P9;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cSharpIntroWinForms.P8
+{
+    public partial class KorisniciPolozeniPredmeti : Form
+    {
+        private Korisnik korisnik;
+
+        KonekcijaNaBazu konekcijaNaBazu = DLWMS.DB;
+
+        private List<string> ocjene = new List<string> {  "6", "7", "8", "9", "10" };
+        public KorisniciPolozeniPredmeti()
+        {
+            InitializeComponent();
+            dgvPolozeniPredmeti.AutoGenerateColumns = false;
+            cmbPredmeti.DataSource = konekcijaNaBazu.Predmeti.ToArray();
+            cmbOcjene.DataSource = ocjene;
+        }
+
+        public KorisniciPolozeniPredmeti(Korisnik korisnik) : this()
+        {
+            this.korisnik = korisnik;
+            UcitajPodatke(this.korisnik);
+        }
+
+        private void KorisniciPolozeniPredmeti_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void UcitajPodatke(Korisnik korisnik, List<KorisniciPredmeti> listaNepolozenih = null)
+        {
+            dgvPolozeniPredmeti.DataSource = null;
+            dgvPolozeniPredmeti.DataSource = listaNepolozenih ?? konekcijaNaBazu.KorisniciPredmeti.Where(x => x.Korisnik.Id == korisnik.Id).ToList();
+        }
+
+        private void cbUcitajNepolozene_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbUcitajNepolozene.Checked == true)
+                UcitajNepolozene();
+            else
+                UcitajPodatke(this.korisnik);
+        }
+
+        private void UcitajNepolozene()
+        {
+            
+            var novaLista = new List<KorisniciPredmeti>();
+            foreach (var predmet in konekcijaNaBazu.Predmeti.ToList())
+                foreach (var predmetKorisnik in korisnik.Uspjeh)
+                {
+                    if(predmet.Id != predmetKorisnik.Id)
+                    {
+                        novaLista.Add(new KorisniciPredmeti
+                        {
+                            Korisnik = korisnik,
+                            Predmet = predmet,
+                            Datum = DateTime.Now.ToString()
+                        });
+                    }
+                }
+            if (!provjeri(novaLista))
+            {
+                cbUcitajNepolozene.Checked = false;
+                return;
+            }
+            UcitajPodatke(this.korisnik,novaLista);
+        }
+
+        private bool provjeri(List<KorisniciPredmeti> novaLista)
+        {
+            int brojac = 0;
+            foreach (var predmet in novaLista)
+            {
+                foreach (var korisnikPredmet in korisnik.Uspjeh)
+                {
+                    if(predmet.Predmet.Naziv == korisnikPredmet.Predmet.Naziv)
+                       brojac++;
+                }
+                if (brojac == 2)
+                {
+                    MessageBox.Show("Korisnik polozio sve ispite!", "INFO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void btnDodajPolozeni_Click(object sender, EventArgs e)
+        {
+            var korPred = new KorisniciPredmeti()
+            {
+                Korisnik = this.korisnik,
+                Predmet = cmbPredmeti.SelectedItem as Predmeti,
+                Ocjena = int.Parse(cmbOcjene.Text),
+                Datum = dtpDatumPolaganja.Value.ToString(),
+            };
+            konekcijaNaBazu.KorisniciPredmeti.Add(korPred);
+            konekcijaNaBazu.SaveChanges();
+            MessageBox.Show("Uspjesno dodano","INFO",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            UcitajPodatke(this.korisnik);
+        }
+
+        private async void btnASYNC_Click(object sender, EventArgs e)
+        {
+            Predmeti predmeti = cmbPredmeti.SelectedItem as Predmeti;
+            Action action = () => UcitajPodatke(this.korisnik);
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 50; i++)
+                {
+                    var kopre = new KorisniciPredmeti()
+                    {
+                        Korisnik = this.korisnik,
+                        Predmet = predmeti,
+                        Ocjena = 6,
+                        Datum = DateTime.Now.ToString(),
+                    };
+                    konekcijaNaBazu.KorisniciPredmeti.Add(kopre);
+                }
+                konekcijaNaBazu.SaveChanges();
+                MessageBox.Show("Uspjesno dodano 50 predmeta", "INFO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                BeginInvoke(action);
+            });
+        }
+    }
+}
